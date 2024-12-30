3750dd9c1c1f99bd46a5f976511b35ca00417947
alba-97 <alvarezbasilio97@gmail.com>
2024-12-30 07:45:07 -0300

    Change profile photo

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,10 +5,6 @@ import projectTwo from "./images/projects/projectTwo.png";
 import projectThree from "./images/projects/projectThree.png";
 import projectFour from "./images/projects/projectFour.png";
 import projectFive from "./images/projects/projectFive.png";
-import testimonialOne from "./images/testmonial/testimonialOne.jpg";
-import testimonialTwo from "./images/testmonial/testimonialTwo.jpeg";
-import testimonialThree from "./images/testmonial/testimonialThree.jpeg";
-import quote from "./images/testmonial/quote.png";
 import contactImg from "./images/contact/contactImg.png";
 
 export {
@@ -19,9 +15,5 @@ export {
   projectThree,
   projectFour,
   projectFive,
-  testimonialOne,
-  testimonialTwo,
-  testimonialThree,
-  quote,
   contactImg,
 };
